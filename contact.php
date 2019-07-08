85a505c81c73c1bc9ab37f1f8dd08c096eaf19f0
Ansel Smolund <smolu006@umn.edu>
2019-07-08 11:39:45 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php
+if($_POST["message"]) {
+    $recipient="anssmolund@gmail.com";
+    $subject="CorTactics inquiry";
+    $sender=$_POST["sender-name"];
+    $senderEmail=$_POST["sender-email"];
+    $message=$_POST["main-message"];
+
+    mail($recipient, $subject, $message, "From: $sender<$senderEmail>");
+}
+?>
