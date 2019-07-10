22b99bd597a9fe1594a3fc9991f0b5dbc5010e51
Ansel Smolund <anssmolund@gmail.com>
2019-07-10 11:17:08 -0500

    Fixed all footer links

--- main-script.js
+++ main-script.js
@@ -1,14 +1,54 @@
-window.onload = function(){
-    var mouseInVideo = false;
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
 
-    console.log("loaded");
+var timeout = null;
+$(document).on('mousemove', function() {
+    if (timeout !== null) {
+        clearTimeout(timeout);
+    }
+    timeout = setTimeout(function() {
+        $('.navbar').css({'transform' : 'translateY(0)'});
+        $('#title-header').css({'opacity' : '1'});
+    }, 1000);
+});
+$(function ()
+{
+    $("#main-cover").mouseenter(function () {
 
-    document.getElementById("main-cover").addEventListener("mouseenter", function(){
-        mouseInVideo = true;
-        console.log(mouseInVideo);
+
+        $('#bgVideo').css({'transform': 'scale(1.8)'});
+    }).mouseleave(function (){
+        $('#bgVideo').css({'transform': 'scale(1.2)'});
+    }).mousemove(function(e){
+        $('.navbar').css({'transform' : 'translateY(-100%)'});
+        $('#title-header').css({'opacity' : '.7'});
+        $('#bgVideo').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()/1.1) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()/1.1) * 100 +'%'});
     });
-    document.getElementById("main-cover").addEventListener("mouseour", function(){
-        mouseInVideo = false;
-        console.log(mouseInVideo);
-    });
-}
+});
+
+$(document).scroll(function() {
+    $('.navbar').css({'transform' : 'translateY(0)'});
+});
+
+// $('.tile')
+// // tile mouse actions
+// .on('mouseover', function(){
+//   $(this).children('.photo').css({'transform': 'scale('+ $(this).attr('data-scale') +')'});
+// })
+// .on('mouseout', function(){
+//   $(this).children('.photo').css({'transform': 'scale(1)'});
+// })
+// .on('mousemove', function(e){
+//   $(this).children('.photo').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
+// })
+// // tiles set up
+// .each(function(){
+//   $(this)
+//     // add a photo container
+//     .append('<div class="photo"></div>')
+//     // some text just to show zoom level on current item in this example
+//     .append('<div class="txt"><div class="x">'+ $(this).attr('data-scale') +'x</div>ZOOM ON<br>HOVER</div>')
+//     // set up a background image for each tile based on data-image attribute
+//     .children('.photo').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
+// })
--- services-script.js
+++ services-script.js
@@ -1,8 +1,26 @@
-function closeNavBar(){
-    document.getElementById("services-nav").style.display = "none";
-}
-function openNavBar(){
-    document.getElementById("services-nav").style.display = "inline-block";
+$( document ).ready(function() {
+    var scroll = $(window).scrollTop();
+    if(scroll > 500){
+        $("#services-nav").css({'top':'-200px'});
+    }
+
+});
+
+$(document).scroll(function() {
+    $("#services-nav").css({'top':'0px'});
+});
+
+function showDiv(divToShow){
+    console.log(document.getElementById(divToShow).style.display);
+    if(document.getElementById(divToShow).style.display == 'none'){
+      console.log("showing");
+      document.getElementById(divToShow).style.display = "block";
+      document.getElementById(divToShow+"_btn").innerHTML = "Show Less";
+    }
+    else if(document.getElementById(divToShow).style.display == "block"){
+      document.getElementById(divToShow).style.display = "none";
+      document.getElementById(divToShow+"_btn").innerHTML = "Show More";
+    }
 }
 
 
